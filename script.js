79a9f75a3a279c6f80d7064cf3a8443fffa54473
sterin <sterinjacob2003@gmail.com>
2025-06-16 19:07:45 +0530

    changes

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ menuIcon.onclick = () =>{
     menuIcon.classList.toggle("bx-x")
     navbar.classList.toggle("active")
 }
+
+//////
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form"); // Selects the first <form> on the page
+    const notification = document.getElementById("notification");
+
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+       console.log("Form submitted!");
+
+      // Show notification
+      notification.style.display = "block";
+
+      // Hide after 3 seconds
+      setTimeout(() => {
+        notification.style.display = "none";
+      }, 3000);
+
+      // Clear the form
+      form.reset();
+    });
+  });
+
+
+
+
